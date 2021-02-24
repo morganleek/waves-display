2bc9de592c37cc1e947ef21a3d7da80230256368
morganleek <morganleek@gmail.com>
2021-02-24 02:32:11 +0000

    Button labels

--- src/js/modules/chart/chart.js
+++ src/js/modules/chart/chart.js
@@ -6,14 +6,16 @@ import { wadMapLocator } from '../map';
 import moment from 'moment';
 
 const panelWrapper = "<div class='panel panel-primary'>" +
-  "<div class='panel-heading clearfix'><h5>{{ buoyLabel }}</h5></div>" + 
-  "<div class='panel-body'>" + 
-    "<div class='chart-js-menu'>" + 
-      "<button class='download-trigger' data-buoy-id='{{ buoyId }}'></button>" +
-      "<button class='maps-trigger' data-buoy-lat='{{ buoyLat }}' data-buoy-lng='{{ buoyLng }}'></button>" +
-      "<button class='calendars-trigger' data-buoy-id='{{ buoyId }}' data-buoy-start='{{ buoyStartTime }}' data-buoy-end='{{ buoyEndTime }}'></button>" +
-    "</div>" +
-    "<div class='canvas-wrapper loading'>" +
+  "<div class='panel-heading clearfix'>" +
+		"<h5 class='pull-left'>{{ buoyLabel }}</h5>" + 
+		"<div class='btn-group btn-group-sm chart-js-menu pull-right' role='group' aria-label='Chart Tools'>" + 
+			"<button class='download-trigger btn btn-default' data-buoy-id='{{ buoyId }}'><i class='fa fa-floppy-o' aria-hidden='true'></i>&nbsp;&nbsp;Export</button>" +
+			"<button class='maps-trigger btn btn-default' data-buoy-lat='{{ buoyLat }}' data-buoy-lng='{{ buoyLng }}'><i class='fa fa-crosshairs' aria-hidden='true'></i>&nbsp;&nbsp;Centre</button>" +
+			"<button class='calendars-trigger btn btn-default' data-buoy-id='{{ buoyId }}' data-buoy-start='{{ buoyStartTime }}' data-buoy-end='{{ buoyEndTime }}'><i class='fa fa-calendar' aria-hidden='true'></i>&nbsp;&nbsp;Date</button>" +
+		"</div>" +
+	"</div>" + 
+	"<div class='panel-body'>" + 
+    "<div class='canvas-wrapper loading clearfix'>" +
       "<canvas></canvas>" +
     "</div>" +
     "<h5 class='latest-observations'>Latest Observations <time></time></h5>" +
