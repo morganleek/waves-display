06bbc730a20215d46353006d48838bfc9f88680b
Morgan Leek <morganleek@Morgans-MacBook-Pro.local>
2021-04-23 22:29:12 +0800

    maps refactor

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,2 +1,7 @@
-import './modules';
+// Load Charts
+import './chart';
+
+// Load Maps
+import './map';
+
 import '../../node_modules/bootstrap/js/src/modal';
---
+++ src/js/overlay/index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import { wadDrawMap } from '../map';
+
+// $( function() {
+//   if( document.getElementsByClassName('page-template---templateswave-overlay-test-php').length ) {
+// 		// Fetch all buoys
+// 		$.ajax({
+// 			type: 'POST',
+// 			url: wad.ajax,
+// 			data: { action: 'waf_rest_list_buoys' },
+// 			success: wadDrawMap, // Process list received
+// 			dataType: 'json'
+// 		});
+// 	}
+// } );
+
+// export function wadOverlayProcessBuoys( response ) {
+//   // Draw map
+//   if( document.getElementById( 'map' ) ) {
+//     wadDrawMap( response );
+//   }
+// }
