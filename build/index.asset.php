f7c87bdc3e479470429e9d2f37322a178214a805
morganleek <morganleek@gmail.com>
2023-11-15 15:15:32 +0800

    backend appearance

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3916350b3164a70eaf3f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5ca6bbd06023b88a1893');
